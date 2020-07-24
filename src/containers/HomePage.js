f306fc391704d79de36eb208ddc34b73c145e309
lhebia <lhebia@gmail.com>
2020-07-23 23:56:14 -0400

    Succesfully sent out API call, returned items to state

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -2,29 +2,27 @@ import React, { useState, useEffect } from 'react'
 
 export default function HomePage() {
 
-    // const [movieState, setMovieState] = useState({ movies:[] }); 
+    const [movieState, setMovieState] = useState({ movies:[] }); 
 
-    useEffect( () => {
-        try {
-            const movieTrendingUrl = `https://api.themoviedb.org/3/trending/movie/day?page=1`;
-            const responsePromise = fetch(movieTrendingUrl, {
-                headers: {
-                    Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
-                    "Content-Type": "application/json;charset=utf-8",
+    useEffect(() => {
+        fetch("https://api.themoviedb.org/3/trending/movie/day?page=1", {
+            headers: {
+                Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+                "Content-Type": "application/json;charset=utf-8",
+            },
+        })
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result.results);
+                    setMovieState(result.results);
                 },
-            });
-            const response = responsePromise.json();
 
-            console.log(response);
-            console.log('Nothing');
-
-            // setMovieState({
-            //     movies: response.results,
-            // });
-        } catch (e) {
-            console.log(e);
-        }
-    })
+                (error) => {
+                    console.log(error);
+                }
+            )
+    }, [])
 
     return (
         <div>
